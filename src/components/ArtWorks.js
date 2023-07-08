bcae4e4c7bd90256b4b300b66a76304fb22ee33b
Paul Dimitris Kyriou <pdkyriou@gmail.com>
2023-07-08 20:35:29 +0100

    w8d40_homework_first commit

--- src/components/ArtWorks.js
+++ src/components/ArtWorks.js
@@ -19,15 +19,16 @@ const ArtWorks = [
     { id: 15, title: 'HELENA', image: 'HELENA.jpg', price: 4350 },
     
     { id: 16, title: 'HENDRIX', image: 'HENDRIX.jpg', price: 10000 },
-    { id: 17, title: 'LOGAN', image: 'LOGAN.jpg', price: 1200 },
-    { id: 18, title: 'LOVERS I', image: 'LOVERS I.jpg', price: 1000 },
-    { id: 19, title: 'LOVERS II', image: 'LOVERS II.jpg', price: 15000 },
+    { id: 17, title: 'LOGAN', image: 'LOGAN.jpg', price: 11200 },
+    { id: 18, title: 'LOVERS I', image: 'LOVERS I.jpg', price: 100000 },
+    { id: 19, title: 'LOVERS II', image: 'LOVERS II.jpg', price: 13000 },
     { id: 20, title: 'LOVERS III', image: 'LOVERS III.jpg', price: 12600 },
     
-    { id: 21, title: 'LOVERS IV', image: 'LOVERS IV.jpg', price: 9350 },
-    { id: 22, title: 'MONROE', image: 'MONROE.jpg', price: 8000 },
-    { id: 23, title: 'PAULA', image: 'PAULA.jpg', price: 25000 },
-    { id: 24, title: 'ROSE', image: 'ROSE.jpg', price: 950 },
+    { id: 21, title: 'LOVERS IV', image: 'LOVERS IV.jpg', price: 19350 },
+    { id: 22, title: 'MONROE', image: 'MONROE.jpg', price: 58000 },
+    { id: 23, title: 'PAULA', image: 'PAULA.jpg', price: 46000 },
+    { id: 24, title: 'ROSE', image: 'ROSE.jpg', price: 9150 },
+    { id: 25, title: 'ROSITA', image: 'ROSITA.jpg', price: 19950 },
 
   ];
   
